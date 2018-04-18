f83ef75ff5bb76b9e098043a68fb29e689f11f1a
Olivier Haas <git.oshaas@dfgh.net>
2018-04-18 11:48:18 +0200

    Conventions de code (package gui.util)

--- Olivier/src/haas/olivier/gui/util/ActionCopy.java
+++ Olivier/src/haas/olivier/gui/util/ActionCopy.java
@@ -12,7 +12,8 @@ import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.ImageIcon;
 
-/** Une action qui permet de copier un texte vers le presse-papiers.
+/**
+ * Une action qui permet de copier un texte vers le presse-papiers.
  * <p>
  * Le texte à copier est paramétrable via une méthode abstraite.
  * 
@@ -24,7 +25,8 @@ public abstract class ActionCopy extends AbstractAction {
 	private static final ImageIcon ICON_COPY = IconLoader.createImageIcon(
 			ActionCopy.class, "../images/sc_copy.png", null);
 	
-	/** Construit une action qui copie un texte vers le presse-papiers.
+	/**
+	 * Construit une action qui copie un texte vers le presse-papiers.
 	 * <p>
 	 * L'action utilise une icône "Copier" imposée.
 	 * 
@@ -32,23 +34,22 @@ public abstract class ActionCopy extends AbstractAction {
 	 */
 	public ActionCopy(String description) {
 		super(description, ICON_COPY);
-	}// constructeur
+	}
 	
-	/** Renvoie le texte à copier vers le presse-papiers. */
+	/**
+	 * Renvoie le texte à copier vers le presse-papiers.
+	 */
 	protected abstract String getTextToCopy();
 
-	/** Copie le texte renvoyé par <code>getTextToCopy()</code> vers le
+	/**
+	 * Copie le texte renvoyé par <code>getTextToCopy()</code> vers le
 	 * presse-papiers.
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		// Contenu texte 
 		StringSelection content = new StringSelection(getTextToCopy());
-		
-		// Placer dans le presse-papiers
-		Toolkit.getDefaultToolkit().getSystemClipboard()
-		.setContents(content, content);
-	}// actionPerformed
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(
+				content, content);
+	}
 
 }
