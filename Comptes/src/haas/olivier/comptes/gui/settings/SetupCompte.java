9b3b15f9cbf5cd5ca8494e70f653a154b3fc8fa8
Olivier Haas <git.oshaas@dfgh.net>
2018-04-30 15:09:02 +0200

    Externalisation et refactorisation de CompteController

--- Comptes/src/haas/olivier/comptes/gui/settings/SetupCompte.java
+++ Comptes/src/haas/olivier/comptes/gui/settings/SetupCompte.java
@@ -650,6 +650,7 @@ public class SetupCompte {
 			if (!controller.isModified())
 				continue;
 			
+			// Dates du compte avant les changements
 			Compte compte = controller.getCompte();
 			Date oldOuverture = null;
 			Date oldCloture = null;
@@ -658,22 +659,14 @@ public class SetupCompte {
 				oldCloture = compte.getCloture();
 			}
 			
-			try {
-				compte = controller.applyChanges();
+			// Nouveau compte (même instance qu'avant, sauf s'il était null)
+			compte = controller.applyChanges();
 
-				// Noter de mettre à jour les bases en cas de changement
-				toUpdate = getOlderMonth(toUpdate, oldOuverture);
-				toUpdate = getOlderMonth(toUpdate, oldCloture);
-				toUpdate = getOlderMonth(toUpdate, compte.getOuverture());
-				toUpdate = getOlderMonth(toUpdate, compte.getCloture());
-
-			} catch (IOException e1) {
-				LOGGER.severe(
-						"Impossible de sauvegarder le compte " + controller);
-
-				// Arrêter ici sans recharger les données
-				return;
-			}
+			// Noter de mettre à jour les bases en cas de changement
+			toUpdate = getOlderMonth(toUpdate, oldOuverture);
+			toUpdate = getOlderMonth(toUpdate, oldCloture);
+			toUpdate = getOlderMonth(toUpdate, compte.getOuverture());
+			toUpdate = getOlderMonth(toUpdate, compte.getCloture());
 
 			// Actualiser la sélection si besoin
 			if (controller == selected) {
@@ -782,7 +775,7 @@ public class SetupCompte {
 		CompteController selected = dataMediator.getController();
 		int confirm = JOptionPane.showConfirmDialog(
 				dialog,
-				String.format("Voulez-vous vraiment supprimer le compte\n%s ?",
+				String.format("Voulez-vous vraiment supprimer le compte%n%s ?",
 						selected),
 				"Supprimer un compte",
 				JOptionPane.YES_NO_OPTION);
@@ -793,197 +786,6 @@ public class SetupCompte {
 			} catch (IOException e1) {
 				LOGGER.severe("Impossible de supprimer " + selected);
 			}
-		}
-	}
-}// class SetupCompte
-
-/**
- * Un contrôleur de Compte.<br>
- * Il permet de pré-visualiser les changements à apporter à un compte.
- * <p>
- * Cette classe est externe à <code>SetupCompte</code> pour éviter que celle-ci
- * n'accède aux propriétés privées du contrôleur, au lieu de passer par les
- * setters.
- */
-class CompteController implements Comparable<CompteController> {
-	
-	/**
-	 * Le compte contrôlé.
-	 */
-	private Compte compte = null;
-	
-	/**
-	 * Marqueur de modifications.
-	 */
-	private boolean modified = false;
-	
-	/**
-	 * Propriété à éditer.<br>
-	 * La valeur par défaut est celle à utiliser pour une nouvelle saisie.
-	 */
-	private String nom		= null;
-	private Color color		= null;
-	private TypeCompte type	= TypeCompte.COMPTE_COURANT;
-	private long numero		= 0L;
-	private Date ouverture	= null;
-	private Date cloture	= null;
-	
-	/**
-	 * Construit un contrôleur contenant les données actuelles du compte
-	 * spécifié.
-	 * 
-	 * @param compte	Le compte à éditer. Utiliser <code>null</code> pour un
-	 * 					nouveau compte.
-	 */
-	public CompteController(Compte compte) {
-		this.compte = compte;
-		refresh();
-	}
-	
-	/**
-	 * Rafraîchit les données à partir du compte actuel.
-	 */
-	private void refresh() {
-		if (compte != null) {
-			
-			// Récupérer les propriétés classiques
-			nom = compte.getNom();
-			color = compte.getColor();
-			type = compte.getType();
-			ouverture = compte.getOuverture();
-			cloture = compte.getCloture();
-			
-			// Selon le type du compte (bancaire ou budgétaire)
-			TypeCompte type = compte.getType();
-			if (type.isBancaire()) {
-				numero = compte.getNumero();
-			}
-		}
-	}
-	
-	/**
-	 * Renvoie le compte actuel.
-	 */
-	public Compte getCompte() {
-		return compte;
-	}
-	
-	// Getters
-	public String getNom()		{return nom;}
-	public Color getColor()		{return color;}
-	public TypeCompte getType()	{return type;}
-	public long getNumero()		{return numero;}
-	public Date getOuverture()	{return ouverture;}
-	public Date getCloture()	{return cloture;}
-	
-	// Setters
-	public void setNom(String nom) {
-		this.nom = nom;
-		modified = true;
-	}
-	public void setColor(Color color) {
-		this.color = color;
-		modified = true;
-	}
-	public void setType(TypeCompte type) {
-		this.type = type;
-		modified = true;
-	}
-	
-	/**
-	 * Modifie le numéro.
-	 * 
-	 * @param numeroText	Le numéro, au format texte.
-	 */
-	public void setNumero(String numeroText) {
-		this.numero = Long.parseLong(numeroText);
-		modified = true;
-	}
-	public void setOuverture(Date ouverture) {
-		this.ouverture = ouverture;
-		modified = true;
-	}
-	public void setCloture(Date cloture) {
-		this.cloture = cloture;
-		modified = true;
-	}
-	
-	/**
-	 * Indique si les données ont été modifiées par rapport à l'état d'origine
-	 * du compte.
-	 */
-	public boolean isModified() {
-		return modified;
-	}
-	
-	/**
-	 * Applique les changements au compte et envoie la nouvelle instance au DAO.
-	 * 
-	 * @return	Le nouveau compte enregistré dans le DAO. S'il n'y a pas de
-	 * 			modifications, c'est la même instance qu'auparavant.
-	 * 
-	 * @throws IOException
-	 */
-	public Compte applyChanges() throws IOException {
-		if (!modified)								// Pas de modifications
-			return compte;							// Ne rien faire
-		
-		// Instancier un nouveau Compte si besoin
-		if (compte == null) {
-			compte = new Compte(type);
-			DAOFactory.getFactory().getCompteDAO().add(compte);
-		}
-		
-		// Ajuster les nouvelles propriétés
-		compte.setColor(color);
-		compte.setOuverture(ouverture);
-		compte.setCloture(cloture);
-		
-		// Réinitialiser le marqueur de modifications
-		modified = false;
-		
-		// Rafraîchir les données
-		refresh();
-		
-		return compte;
-	}
-	
-	/**
-	 * Supprime le compte.
-	 * 
-	 * @throws IOException
-	 */
-	public void deleteCompte() throws IOException {
-		DAOFactory.getFactory().getCompteDAO().remove(compte);
-	}
-
-	/**
-	 * Applique la relation d'ordre des comptes aux contrôleurs de comptes.<br>
-	 * Si un contrôleur et vide ("Nouveau..."), il passe avant.
-	 */
-	@Override
-	public int compareTo(CompteController controller) {
-		Compte compte2 = controller.getCompte();
-		if (compte != null && compte2 != null) {	// Cas général
-			return compte.compareTo(compte2);
-		} else if (compte == null && compte2 != null) {
-			return -1;								// Le null en premier
-		} else if (compte != null && compte2 == null) {
-			return 1;								// Le non-null en dernier
-		} else {
-			return 0;								// Deux null: égalité !
-		}
-	}
-	
-	/**
-	 * Renvoie le nom du compte vers lequel pointe l'objet.
-	 */
-	@Override
-	public String toString() {
-		if (compte == null) {
-			return "Nouveau...";
-		} else {
-			return compte.toString();
 		}
 	}
 }
