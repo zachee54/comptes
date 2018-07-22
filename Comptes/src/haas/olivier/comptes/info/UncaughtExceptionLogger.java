947a7f20c2b0d2477b64b0ea27dad4c73c83fcd8
Olivier Haas <git.oshaas@dfgh.net>
2018-07-22 09:19:28 +0200

    Conventions de code (Comptes et info.UncaughtExceptionLogger)

--- Comptes/src/haas/olivier/comptes/info/UncaughtExceptionLogger.java
+++ Comptes/src/haas/olivier/comptes/info/UncaughtExceptionLogger.java
@@ -4,7 +4,8 @@ import java.lang.Thread.UncaughtExceptionHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/** Un intercepteur d'exceptions qui renvoie vers le système de logs.
+/**
+ * Un intercepteur d'exceptions qui renvoie vers le système de logs.
  * <p>
  * Cette classe permet d'afficher à l'écran les exceptions non traitées, plutôt
  * que sur la console. Utile pour les programmes graphiques, puisque la console
@@ -14,13 +15,15 @@ import java.util.logging.Logger;
  */
 public class UncaughtExceptionLogger implements UncaughtExceptionHandler {
 
-	/** Le Logger pour cette classe. */
+	/**
+	 * Le Logger pour cette classe.
+	 */
 	private static final Logger LOGGER =
 			Logger.getLogger(UncaughtExceptionLogger.class.getName());
 	
 	@Override
 	public void uncaughtException(Thread t, Throwable e) {
 		LOGGER.log(Level.SEVERE, "Erreur à l'exécution", e);
-	}// uncaughtException
+	}
 
 }
