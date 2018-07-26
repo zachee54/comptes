07895d59c06e2be87f4e43dd2405bab2063e8929
Olivier HAAS <olivier-1.haas@dgfip.finances.gouv.fr>
2018-07-26 14:46:44 +0200

    Utiliser une WeakHashMap pour le pool

---
+++ src/test/java/haas/olivier/util/PoolTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2018 DGFiP - Tous droits réservés
+ * 
+ */
+package haas.olivier.util;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PoolTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	
+	/**
+	 * Objet testé.
+	 */
+	private final Pool pool = new Pool();
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGet() {
+		Object a = new Integer(12);
+		Object b = new Integer(12);
+		Object c = Integer.valueOf(7);
+		assertNotSame(a, b);
+		
+		assertSame(a, pool.get(a));		// Première insertion de a
+		assertSame(c, pool.get(c));		// Première insertion de c
+		assertSame(a, pool.get(b));		// Insertion de b : renvoyer a égal à b
+	}
+
+}
