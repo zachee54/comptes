c8fda5f09e682462266e3394c31e176424b02203
Olivier HAAS <olivier-1.haas@dgfip.finances.gouv.fr>
2018-11-13 11:46:58 +0100

    Refactorisation de IconLoader
    Chemins absolus des images (plutôt que relatifs à une classe spécifiée)
    Utilisation d'un log plutôt que System.err
    Renommage des méthodes
    Constructeur privé

--- src/main/java/haas/olivier/gui/IconLoader.java
+++ src/main/java/haas/olivier/gui/IconLoader.java
@@ -3,50 +3,46 @@
  */
 package haas.olivier.gui;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.swing.ImageIcon;
 
-/** Classe statique pour le chargement des images.
+/**
+ * Classe statique pour le chargement des images.
  * 
  * @author Olivier HAAS
  */
 public class IconLoader {
 	
-	/** Charge une icône à partir du répertoire de cette classe.
+	private IconLoader() {
+	}
+	
+	/**
+	 * Charge une icône si elle est disponible.
+	 * 
+	 * @param path	Le chemin de l'image à charger.
+	 * @return		Une icône.
+	 */
+	public static ImageIcon loadIcon(String path) {
+		return loadIcon(path, null);
+	}
+	
+	/**
+	 * Charge une icône si elle est disponible.
 	 * 
 	 * @param path			Le chemin de l'image à charger.
 	 * @param description	La description à donner à l'image.
 	 * @return				Une icône.
 	 */
-	public static ImageIcon createImageIcon(String path, String description) {
-		return createImageIcon(IconLoader.class, path, description);
-	}
-	
-	/** Charge une icône.
-	 * 
-	 * @param clazz			La classe de référence pour le chemin de l'image.
-	 * 						En effet, si <code>path</code> est un chemin
-	 * 						relatif, il faut savoir dans quel répertoire on se
-	 * 						place. Il s'agit alors du répertoire de la classe de
-	 * 						l'objet <code>source</code>.
-	 * 						Si <code>path</code> est un chemin absolu, alors ce
-	 * 						paramètre n'a pas d'importance. Néanmoins, il ne
-	 * 						peut pas être <code>null</code>.
-	 * 						
-	 * @param path			Le chemin de l'image à charger.
-	 * 
-	 * @param description	La description à donner à l'image.
-	 * 
-	 * @return				Une icône, ou <code>null</code> si l'icône n'a pas
-	 * 						pu être chargée.
-	 */
-	public static ImageIcon createImageIcon(Class<?> clazz, String path,
-			String description) {
-		java.net.URL imgURL = clazz.getResource(path);
-		if (imgURL != null) {
-			return new ImageIcon(imgURL, description);
-		} else {
-			System.err.println("Impossible de charger l'image " + path);
+	public static ImageIcon loadIcon(String path, String description) {
+		java.net.URL imgURL = IconLoader.class.getResource(path);
+		if (imgURL == null) {
+			Logger.getLogger(IconLoader.class.getName()).log(
+					Level.CONFIG, "Impossible de charger l''image {0}", path);
 			return null;
+		} else {
+			return new ImageIcon(imgURL, description);
 		}
-	}// createImageIcon
+	}
 }
