c18f9fe119f8a7a0c18f5e4c4d3f0cdd7a4ba01a
Olivier Haas <git.oshaas@dfgh.net>
2019-02-10 21:51:48 +0100

    Création de PermanentState

---
+++ Comptes/src/haas/olivier/comptes/PermanentState.java
@@ -0,0 +1,32 @@
+package haas.olivier.comptes;
+
+import java.math.BigDecimal;
+
+import haas.olivier.util.Month;
+
+/**
+ * L'interface d'état des opérations permanentes. Elle détermine la façon de
+ * définir une nouvelle écriture.
+ *
+ * @author Olivier Haas
+ */
+public interface PermanentState {
+	
+	/**
+	 * Renvoie le montant de l'écriture à générer.
+	 * 
+	 * @param month	Le mois au titre duquel générer l'écriture.
+	 * 
+	 * @throws EcritureMissingArgumentException
+	 * 				Si les données sont insuffisantes pour instancier
+	 * 				l'écriture.
+	 * 
+	 * @throws InconsistentArgumentException
+	 * 				Si des informations manquent pour définir le montant au
+	 * 				titre de ce mois.
+	 */
+	BigDecimal getMontant(Month month)
+			throws EcritureMissingArgumentException,
+			InconsistentArgumentsException;
+
+}
