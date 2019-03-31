30988b50578c0289b85e27bed5fd0ba2ec312c1e
Olivier Haas <git.oshaas@dfgh.net>
2019-03-31 19:12:54 +0200

    Déplacer les répertoires source

--- Olivier/src/haas/olivier/gui/popup/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Popups animés apparaissant en haut d'un panneau.
- * 
- * @author Olivier HAAS
- */
-package haas.olivier.gui.popup;
--- Olivier/src/haas/olivier/gui/util/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Outils personnels divers pour Swing.
- * <p>
- * Date: 7 févr. 2018
- * @author Olivier HAAS
- */
-package haas.olivier.gui.util;
